48667c5fe105d04c14878155be440b39355f4e37
balthair94 <archerbaltazar@gmail.com>
2017-03-27 22:58:15 -0600

    koan about_triangle_project.rb completed

--- triangle.rb
+++ triangle.rb
@@ -14,7 +14,13 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
-  # WRITE THIS CODE
+  if [a,b,c].max > 0
+  	return :equilateral if a == b && b == c
+  	return :isosceles if a == b || b == c || a == c
+  	return :scalene if a != b && b != c
+  else
+  	raise TriangleError
+  end
 end
 
 # Error class used in part 2.  No need to change this code.
